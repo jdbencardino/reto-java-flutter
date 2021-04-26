7f3969e165836babafe47f1ca8ec2bc555b4b471
jdbencardino <juandiego.bencardino@ibm.com>
2021-04-26 14:43:57 -0500

    Añadir archivos generados por spring initializr
    Se añadieron los archivos para realizar un poryecto con spring boot generados por spring initializr con las dependencias de Spring web, spring data jpa, entre otras.

---
+++ apppeliculas-back/src/main/java/com/ibm/javaflutter/apppeliculasback/ApppeliculasBackApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.javaflutter.apppeliculasback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApppeliculasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApppeliculasBackApplication.class, args);
+	}
+
+}
---
+++ apppeliculas-back/src/test/java/com/ibm/javaflutter/apppeliculasback/ApppeliculasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.javaflutter.apppeliculasback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApppeliculasBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
