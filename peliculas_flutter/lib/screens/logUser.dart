59c79a3403eeb13224e1f75e6570348aa4f49672
jdbencardino <juandiego.bencardino@ibm.com>
2021-05-01 09:15:44 -0500

    Habilitar login user

--- peliculas_flutter/lib/screens/logUser.dart
+++ peliculas_flutter/lib/screens/logUser.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:peliculas_flutter/screens/logAdmin.dart';
 import 'package:peliculas_flutter/widgets/basedWidgets.dart';
 import 'package:peliculas_flutter/helpers/constantes.dart';
 import 'package:http/http.dart' as http;
@@ -20,21 +21,43 @@ class _LogUserState extends State<LogUser> {
       appBar: AppBar(
         title: Text('Entrar como suscriptor'),
       ),
-      body: kBasedLoginWidget('Log User', context, onClickLog),
+      body: kBasedLoginWidget('Log User', context, onClick),
     );
   }
 }
 
-Future<void> onClickLog(context, email, password) async {
-  FirebaseAuth _auth = await FirebaseAuth.instance;
+void onClick(context, email, password) async {
+  //Firebase.initializeApp();
+  FirebaseAuth _auth = FirebaseAuth.instance;
 
   try {
     final mAuth = await _auth.signInWithEmailAndPassword(
       email: email,
       password: password,
     );
+    if (mAuth != null) {
+      String uid = mAuth.user.uid;
+      String url =
+          'http://localhost:8080/subscribers/search/findByUid?uid=${uid}';
+      Uri link = Uri.parse(url);
+      var respuesta = await http.get(
+        link,
+        headers: {'Content-Type': 'application/json'},
+      );
 
-    continueWithLogin(mAuth, context);
+      respuesta.statusCode < 400
+          ? Fluttertoast.showToast(
+              msg: 'Bienvenido Subscriber',
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.CENTER,
+              timeInSecForIosWeb: 1,
+              fontSize: 16.0)
+          : print(respuesta.statusCode);
+
+      respuesta.statusCode < 400
+          ? Navigator.pushNamed(context, mainScreenSub)
+          : print(respuesta.statusCode);
+    }
   } catch (e) {
     print(e);
   }
